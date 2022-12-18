2d17899b66a3f7bbdead1582152a031b13f96885
Megan McKissack <meganmckissack@gmail.com>
2022-12-17 19:27:36 -0800

    create build files

--- src/Experience.js
+++ src/Experience.js
@@ -8,54 +8,54 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 function Experience() {
   const cubeRef = useRef();
   const groupRef = useRef();
-  const model = useLoader(GLTFLoader, './tree-stump.glb');
-  console.log(model);
+  // const model = useLoader(GLTFLoader, './tree-stump.glb');
+  // console.log(model);
 
   useFrame((state, delta) => {
     // state contains information about our Three.js environment like the camera, the renderer, the scene, etc
     // delta contains the time spent since the last frame in seconds
     // console.log(delta)
-    // cubeRef.current.rotation.y += delta
+    cubeRef.current.rotation.y += delta
     // groupRef.current.rotation.y += delta
   });
 
 
   return (
     <React.Fragment>
-      {/* <mesh rotation-y={ Math.PI * 0.25 } position-x={2} scale={ 1.5 }>
+      <mesh rotation-y={ Math.PI * 0.25 } position-x={2} scale={ 1.5 }>
           {/* parameters are radius, widthSegments, heightSegments - same order as Three.js */}
-          {/* <sphereGeometry args={ [1.5, 32, 32 ]} />   */}
+          <sphereGeometry args={ [1.5, 32, 32 ]} />  
           <boxGeometry />
           {/* MeshBasicMaterial requires only one parameter and it’s an object */}
-          <meshBasicMaterial color="purple" wireframe />
+          {/* <meshBasicMaterial color="purple" wireframe /> */}
           {/* Writing color="mediumpurple" is like writing material.color.set('mediumpurple')
           Writing wireframe without any value is like setting it to true. We can still set it to false if needed:  */}
-      {/* </mesh> */ }
+       </mesh> 
 
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
 
-      {/* <group ref={ groupRef }>
+      <group ref={ groupRef }>
         <mesh position-x={ - 2 }>
           <sphereGeometry />
-          <meshBasicMaterial color="orange" />
+          <meshBasicMaterial color="green" />
         </mesh>
 
         <mesh ref={ cubeRef } rotation-y={ Math.PI * 0.25 } position-x={ 2 } scale={ 1.5 }>
           <boxGeometry />
           <meshBasicMaterial color="mediumpurple" />
         </mesh>
-      </group> */}
+      </group>
 
       <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
       <ambientLight intensity={ 0.5 } />
 
-      <primitive object={ model.scene } />
+      {/* <primitive object={ model.scene } /> */}
 
       <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
         <planeGeometry />
-        <meshBasicMaterial color="greenyellow" />
+        <meshBasicMaterial color="hotpink" />
       </mesh>
     </React.Fragment>
   )
